3b169c797503b20b259eac31f65069121c27f241
LamTruong-SysOps <truong@myg.vn>
2025-04-03 10:14:56 +0700

    keep 700 logs

--- backup_gitlab_MinIO.sh
+++ backup_gitlab_MinIO.sh
@@ -4,7 +4,7 @@
 GITLAB_BACKUP="/var/opt/gitlab/backups"
 GITLAB_ETC="/etc/gitlab/config_backup"
 
-GITLAB_TMP="/root/backup"
+GITLAB_TMP="/root/backup/gitlab"
 BACKUP_DATE=`date '+%F'`
 
 MINIO="myminio/gitlab"
@@ -33,6 +33,7 @@ sudo gitlab-ctl backup-etc >> $LOG_FILE
 # Compress files all in one
 LATEST_BACKUP=$(basename $(ls -Art $GITLAB_BACKUP/*.tar 2>/dev/null | tail -n 1))
 LATEST_ETC=$(basename $(ls -Art $GITLAB_ETC/*.tar 2>/dev/null | tail -n 1))
+
 echo "[4/5] $(date "+%d-%m-%Y_%H:%M:%S") - Compress files all in one..." >> $LOG_FILE
 if [[ -z "$LATEST_BACKUP" || -z "$LATEST_ETC" ]]; then
   echo "Not found file .tar in $GITLAB_BACKUP or $GITLAB_ETC. Exit script." >> $LOG_FILE
@@ -42,6 +43,9 @@ tar -czf $GITLAB_TMP/$BACKUP_DATE.tar.gz -C $GITLAB_BACKUP $LATEST_BACKUP -C $GI
 
 # Push file backup to MinIO
 echo "[5/5] $(date "+%d-%m-%Y_%H:%M:%S") - Push file backup to MinIO..." >> $LOG_FILE
-mc cp $GITLAB_TMP/$BACKUP_DATE.tar.gz $MINIO >> $LOG_FILE
+/usr/local/bin/mc cp $GITLAB_TMP/$BACKUP_DATE.tar.gz $MINIO >> $LOG_FILE
 
-echo "✅ $(date "+%d-%m-%Y_%H:%M:%S") - Done backup GitLab and upload to MinIO" >> $LOG_FILE
+echo "  $(date "+%d-%m-%Y_%H:%M:%S") - Done backup GitLab and upload to MinIO" >> $LOG_FILE
+
+# Keep the last 700 logs
+echo "$(tail -700 $LOG_FILE)" > $LOG_FILE
